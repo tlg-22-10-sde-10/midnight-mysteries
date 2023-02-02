7ebdb2b02b4ff79df76a08e4f34ca8b1ea38ee75
the-mighty-fall <vvarfare@gmail.com>
2023-02-02 11:38:00 -0600

    added jar file & oneclick files, fix AB#331

--- src/main/java/com/game/client/GameClient.java
+++ src/main/java/com/game/client/GameClient.java
@@ -4,7 +4,6 @@ import com.game.menu.StoryTutorial;
 
 public class GameClient {
     public static void main(String[] args) throws InterruptedException {
-
         StoryTutorial.printStory();
     }
 }
