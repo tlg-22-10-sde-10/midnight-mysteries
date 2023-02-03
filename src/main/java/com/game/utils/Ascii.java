0fab5b10bfa156d6c754ba5474e60520fe3217fa
the-mighty-fall <vvarfare@gmail.com>
2023-02-03 13:24:44 -0600

    attempt merge, fix AB#319

--- src/main/java/com/game/utils/Ascii.java
+++ src/main/java/com/game/utils/Ascii.java
@@ -10,12 +10,7 @@ public class Ascii {
     private static final int COLUMNS = 120;
     private ColoredPrinter printer = new ColoredPrinter();
 
-<<<<<<< HEAD
     public static void printTitleBanner() throws InterruptedException {
-=======
-
-    public static void printTitle() throws InterruptedException {
->>>>>>> 940b9856a4447a87e73c579d5189f30e6d7fb8ec
         String centeredBanner = "       ███▄ ▄███▓    ██▓   ▓█████▄     ███▄    █     ██▓     ▄████     ██░ ██    ▄▄▄█████▓        \n" +
                 "      ▓██▒▀█▀ ██▒   ▓██▒   ▒██▀ ██▌    ██ ▀█   █    ▓██▒    ██▒ ▀█▒   ▓██░ ██▒   ▓  ██▒ ▓▒        \n" +
                 "      ▓██    ▓██░   ▒██▒   ░██   █▌   ▓██  ▀█ ██▒   ▒██▒   ▒██░▄▄▄░   ▒██▀▀██░   ▒ ▓██░ ▒░        \n" +
@@ -80,27 +75,10 @@ public class Ascii {
     }
 
     public static void clearTerminal() {
-<<<<<<< HEAD
         try {
             new ProcessBuilder("cmd", "/c", "cls", "clear", "\033[H\033[2J", "\033\143").inheritIO().start().waitFor();
         } catch (Exception e) {
             System.out.println("Error clearing terminal: " + e.getMessage());
-=======
-        String os = System.getProperty("os.name").toLowerCase();
-//        System.out.println(os);
-        if (os.contains("windows")) {
-            try {
-                new ProcessBuilder("cmd", "/c", "cls", "clear", "\033[H\033[2J", "\033\143").inheritIO().start().waitFor();
-            } catch (Exception e) {
-                System.out.println("Error clearing terminal: " + e.getMessage());
-            }
-        } else if (os.contains("mac") || os.contains("os x")) {
-            try {
-                new ProcessBuilder("clear", "/c", "cls", "cmd", "\033[H\033[2J", "\033\143").inheritIO().start().waitFor();
-            } catch (Exception e) {
-                System.out.println("Error clearing terminal: " + e.getMessage());
-            }
->>>>>>> 940b9856a4447a87e73c579d5189f30e6d7fb8ec
         }
     }
 
@@ -161,7 +139,7 @@ public class Ascii {
         System.out.print("\033]0;" + "Midnight Mysteries" + "\007");
     }
 
-    public static void printExitBanner(){
+    public static void printExitBanner() throws InterruptedException {
         clearTerminal();
         String centeredBanner = "  ██████    ▓█████    ▓█████    ▓██   ██▓    ▒█████      █    ██                   \n" +
                 "▒██    ▒    ▓█   ▀    ▓█   ▀     ▒██  ██▒   ▒██▒  ██▒    ██  ▓██▒                  \n" +
@@ -203,9 +181,6 @@ public class Ascii {
                 e.printStackTrace();
             }
         }
-
-
-        System.exit(0);
 
     }
 
